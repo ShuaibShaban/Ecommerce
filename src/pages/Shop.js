30793edb947502482b9f9d6e096467a7a1f7ee60
shuaib shaban <shuaib>
2024-01-18 10:18:14 +0300

    offers component complete

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/popular/Popular'
+import Offers from '../components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
     </div>
   )
 }
