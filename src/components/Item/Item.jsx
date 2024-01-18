5efdf39fa5b0c1d2ea0656ba501f51499ed94a65
shuaib shaban <shuaib>
2024-01-18 09:39:35 +0300

    popular in women component added

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './item.css'
+import './Item.css'
 
 const item = (props) => {
   return (
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/popular/Popular'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   )
 }
